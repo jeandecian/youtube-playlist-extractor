bd0183621544c0275cf8fa451e673e771265bb21
Jean Decian <jeandecian@hotmail.fr>
2024-07-12 00:37:18 -0400

    add videos getter

--- main.py
+++ main.py
@@ -65,6 +65,21 @@ def get_playlist_items(youtube, playlist_id):
     return playlist_items
 
 
+def get_videos(youtube, video_ids):
+    videos = []
+
+    for i in range(0, len(video_ids), 50):
+        response = (
+            youtube.videos()
+            .list(part="contentDetails, snippet", id=",".join(video_ids[i : i + 50]))
+            .execute()
+        )
+
+        videos.extend(response.get("items"))
+
+    return videos
+
+
 def save_to_json(output_file, data):
     with open(output_file, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
@@ -78,3 +93,7 @@ playlists_items = []
 for playlist in playlists:
     playlists_items.extend(get_playlist_items(youtube, playlist.get("id")))
 save_to_json("data/playlists_items.json", {"playlists_items": playlists_items})
+
+video_ids = [item.get("contentDetails").get("videoId") for item in playlists_items]
+videos = get_videos(youtube, video_ids)
+save_to_json("data/videos.json", {"videos": videos})
