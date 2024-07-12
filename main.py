736bc13e6ff914e960e4341add9a24dbe10920ea
Jean Decian <jeandecian@hotmail.fr>
2024-07-12 00:22:56 -0400

    add playlists items getter

--- main.py
+++ main.py
@@ -40,6 +40,31 @@ def get_playlists(youtube):
     return playlists
 
 
+def get_playlist_items(youtube, playlist_id):
+    playlist_items = []
+
+    next_page_token = None
+    while True:
+        response = (
+            youtube.playlistItems()
+            .list(
+                part="contentDetails",
+                playlistId=playlist_id,
+                maxResults=50,
+                pageToken=next_page_token,
+            )
+            .execute()
+        )
+
+        playlist_items.extend(response.get("items"))
+
+        next_page_token = response.get("nextPageToken")
+        if not next_page_token:
+            break
+
+    return playlist_items
+
+
 def save_to_json(output_file, data):
     with open(output_file, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
@@ -48,3 +73,8 @@ def save_to_json(output_file, data):
 youtube = get_authenticated_service(CLIENT_SECRETS_FILE, SCOPES, SERVICE_NAME, VERSION)
 playlists = get_playlists(youtube)
 save_to_json("data/playlists.json", {"playlists": playlists})
+
+playlists_items = []
+for playlist in playlists:
+    playlists_items.extend(get_playlist_items(youtube, playlist.get("id")))
+save_to_json("data/playlists_items.json", {"playlists_items": playlists_items})
